83a7d7ff3a2478038cc659af03c58f7e379f3936
notgiven688 <37874600+notgiven688@users.noreply.github.com>
2023-09-19 22:45:42 +0200

    Update Shader.cs

--- src/JitterDemo/Renderer/OpenGL/Objects/Shader.cs
+++ src/JitterDemo/Renderer/OpenGL/Objects/Shader.cs
@@ -99,7 +99,7 @@ public class Shader : GLObject
         {
             GL.GetShaderInfoLog(Handle, 1024, out int length, out string infoLog);
 #if DEBUG
-            Debug.Fail(infoLog);
+            System.Diagnostics.Debug.Fail(infoLog);
 #endif
             throw new ShaderCompileException(infoLog);
         }
@@ -151,7 +151,7 @@ public class ShaderProgram : GLObject
         {
             GL.GetProgramInfoLog(Handle, 1024, out int _, out string infoLog);
 #if DEBUG
-            Debug.Fail(infoLog);
+            System.Diagnostics.Debug.Fail(infoLog);
 #endif
             throw new ShaderLinkException(infoLog);
         }
