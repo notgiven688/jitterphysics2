dc3d1d93b3794dd40b341cc4df3c24035a2d6f8d
notgiven688 <notgiven688@users.noreply.github.com>
2023-11-12 15:34:55 +0100

    Fix formatting in InertiaTests.cs

--- src/JitterTests/InertiaTests.cs
+++ src/JitterTests/InertiaTests.cs
@@ -24,7 +24,7 @@ public class InertiaTests
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void CylinderInertia()
     {
@@ -32,7 +32,7 @@ public class InertiaTests
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void ConeInertia()
     {
@@ -40,7 +40,7 @@ public class InertiaTests
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void BoxInertia()
     {
@@ -48,7 +48,7 @@ public class InertiaTests
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void SphereInertia()
     {
@@ -56,36 +56,35 @@ public class InertiaTests
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void TransformedInertia()
     {
         var ss = new SphereShape(0.429f);
         var translation = new JVector(2.847f, 3.432f, 1.234f);
-        
+
         var ts = new TransformedShape(ss, translation);
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
+
     [TestCase]
     public static void ConvexHullInertia()
     {
         List<JTriangle> cvh = new List<JTriangle>();
-        
+
         JVector a = new JVector(0.234f, 1.23f, 3.54f);
         JVector b = new JVector(7.788f, 0.23f, 8.14f);
         JVector c = new JVector(2.234f, 8.23f, 8.14f);
         JVector d = new JVector(6.234f, 3.23f, 9.04f);
-        
+
         cvh.Add(new JTriangle(a, b, c));
         cvh.Add(new JTriangle(a, b, d));
         cvh.Add(new JTriangle(b, c, d));
         cvh.Add(new JTriangle(a, c, d));
-        
+
         var ts = new ConvexHullShape(cvh);
         ShapeHelper.CalculateMassInertia(ts, out JMatrix inertia, out JVector com, out float mass, 8);
         Check(ts, inertia, com, mass);
     }
-    
 }
