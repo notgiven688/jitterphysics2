7075dd3b94799d18d965a642ac2d4e268d40011c
notgiven688 <notgiven688@users.noreply.github.com>
2023-11-14 17:14:12 +0100

    moved to nuget package for raylib-cs

--- other/WebDemo/Program.cs
+++ other/WebDemo/Program.cs
@@ -1,50 +1,53 @@
-using System;
-using System.Runtime.InteropServices.JavaScript;
+using System.Numerics;
 using Raylib_cs;
 using static Raylib_cs.Raylib;
 
-Console.WriteLine("Hello, Browser!");
-
-// Initialization
-//--------------------------------------------------------------------------------------
-const int screenWidth = 800;
-const int screenHeight = 450;
-
-
-unsafe
+static Texture2D GenCheckedTexture(int size, int checks, Color colorA, Color colorB)
 {
-    InitWindow(screenWidth, screenHeight, (sbyte*)(0));
+    Image imageMag = GenImageChecked(size, size, checks, checks, colorA, colorB);
+    Texture2D textureMag = LoadTextureFromImage(imageMag);
+    UnloadImage(imageMag);
+    return textureMag;
 }
 
+// Set a hint for anti-aliasing
+SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT);
 
-// Main game loop
-while (!WindowShouldClose())    // Detect window close button or ESC key
+// Initialize a 1200x800 px window with a title
+InitWindow(1200, 800, "BoxDrop Example");
+
+// Dynamically create a plane model
+Texture2D texture = GenCheckedTexture(10, 1,  Color.LIGHTGRAY, Color.GRAY);
+Model planeModel = LoadModelFromMesh(GenMeshPlane(10, 10, 10, 10));
+SetMaterialTexture(ref planeModel, 0, MaterialMapIndex.MATERIAL_MAP_DIFFUSE, ref texture);
+
+// Create a camera
+Camera3D camera = new ()
 {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
+    Position = new Vector3(-20.0f, 8.0f, 10.0f),
+    Target = new Vector3(0.0f, 4.0f, 0.0f),
+    Up = new Vector3(0.0f, 1.0f, 0.0f),
+    FovY = 45.0f,
+    Projection = CameraProjection.CAMERA_PERSPECTIVE
+};
 
-    // Draw
-    //----------------------------------------------------------------------------------
+// Set a target of 100 fps
+SetTargetFPS(100);
+
+// Simple render loop
+while (!WindowShouldClose())
+{
     BeginDrawing();
+    ClearBackground(Color.BLUE);
 
-    ClearBackground(Color.RAYWHITE);
+    BeginMode3D(camera);
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
+    DrawModel(planeModel, Vector3.Zero, 1.0f, Color.WHITE);
+
+    EndMode3D();
+    DrawText($"{GetFPS()} fps", 10, 10, 20, Color.WHITE); 
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
 }
-
 
 CloseWindow();
-
-return 0;
-
-public partial class MyClass
-{
-
-
-
-}
