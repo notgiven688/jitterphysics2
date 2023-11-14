c066bd9b823e1712c0c53829137b7243da60179d
notgiven688 <notgiven688@users.noreply.github.com>
2023-11-14 18:44:56 +0100

    Added default demo

--- other/WebDemo/Program.cs
+++ other/WebDemo/Program.cs
@@ -1,6 +1,21 @@
-using System.Numerics;
+﻿using System.Numerics;
 using Raylib_cs;
+using Jitter2;
+using Jitter2.Collision.Shapes;
+using Jitter2.Dynamics;
+using Jitter2.LinearMath;
 using static Raylib_cs.Raylib;
+
+static Matrix4x4 GetRayLibTransformMatrix(RigidBody body)
+{
+    JMatrix ori = body.Orientation;
+    JVector pos = body.Position;
+
+    return new Matrix4x4(ori.M11, ori.M12, ori.M13, pos.X,
+                         ori.M21, ori.M22, ori.M23, pos.Y,
+                         ori.M31, ori.M32, ori.M33, pos.Z,
+                         0, 0, 0, 1.0f);
+}
 
 static Texture2D GenCheckedTexture(int size, int checks, Color colorA, Color colorB)
 {
@@ -10,18 +25,43 @@ static Texture2D GenCheckedTexture(int size, int checks, Color colorA, Color col
     return textureMag;
 }
 
-// Set a hint for anti-aliasing
+const int NumberOfBoxes = 12;
+
+// set a hint for anti-aliasing
 SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT);
 
-// Initialize a 1200x800 px window with a title
-InitWindow(1200, 800, "BoxDrop Example");
+// initialize a 1200x800 px window with a title
+InitWindow(1200, 800, "BoxDrop example");
 
-// Dynamically create a plane model
+// dynamically create a plane model
 Texture2D texture = GenCheckedTexture(10, 1,  Color.LIGHTGRAY, Color.GRAY);
 Model planeModel = LoadModelFromMesh(GenMeshPlane(10, 10, 10, 10));
 SetMaterialTexture(ref planeModel, 0, MaterialMapIndex.MATERIAL_MAP_DIFFUSE, ref texture);
 
-// Create a camera
+// dynamically create a box model
+texture = GenCheckedTexture(2, 1,  Color.WHITE, Color.MAGENTA);
+Mesh boxMesh = GenMeshCube(1, 1, 1);
+Material boxMat = LoadMaterialDefault();
+SetMaterialTexture(ref boxMat, MaterialMapIndex.MATERIAL_MAP_DIFFUSE, texture);
+
+// initialize the Jitter physics world
+World world = new ();
+
+// add a body representing the plane
+RigidBody planeBody = world.CreateRigidBody();
+planeBody.AddShape(new BoxShape(10));
+planeBody.Position = new JVector(0, -5, 0);
+planeBody.IsStatic = true;
+
+// add NumberOfBoxes cubes
+for(int i = 0; i < NumberOfBoxes; i++)
+{
+    RigidBody body = world.CreateRigidBody();
+    body.AddShape(new BoxShape(1));
+    body.Position = new JVector(0, i * 2 + 0.5f, 0);
+}
+
+// create a camera
 Camera3D camera = new ()
 {
     Position = new Vector3(-20.0f, 8.0f, 10.0f),
@@ -31,10 +71,10 @@ Camera3D camera = new ()
     Projection = CameraProjection.CAMERA_PERSPECTIVE
 };
 
-// Set a target of 100 fps
+// 100 fps target
 SetTargetFPS(100);
 
-// Simple render loop
+// simple render loop
 while (!WindowShouldClose())
 {
     BeginDrawing();
@@ -43,6 +83,14 @@ while (!WindowShouldClose())
     BeginMode3D(camera);
 
     DrawModel(planeModel, Vector3.Zero, 1.0f, Color.WHITE);
+
+    world.Step(1.0f / 100.0f, true);
+
+    foreach(var body in world.RigidBodies)
+    {
+        if(body == planeBody) continue; // do not draw this
+        DrawMesh(boxMesh, boxMat, GetRayLibTransformMatrix(body));
+    }
 
     EndMode3D();
     DrawText($"{GetFPS()} fps", 10, 10, 20, Color.WHITE); 
