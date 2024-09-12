b71b692784ba9dcf76826d00ddbf294e121f8cce
notgiven688 <notgiven688@users.noreply.github.com>
2024-09-12 18:27:43 +0200

    Workaround for freezes using the debugger under Linux

--- src/Jitter2/Parallelization/ThreadPool.cs
+++ src/Jitter2/Parallelization/ThreadPool.cs
@@ -70,6 +70,8 @@ public sealed class ThreadPool
         }
     }
 
+    public const float ThreadsPerProcessor = 0.9f;
+
     // ManualResetEventSlim performs much better than the regular ManualResetEvent.
     // mainResetEvent.Wait() is a 'fallthrough' for the persistent threading model in Jitter.
     // Here the performance improvement of ManualResetEvent is mostly visible.
@@ -80,14 +82,6 @@ public sealed class ThreadPool
     private readonly ConcurrentQueue<ITask> taskQueue = new();
 
     private static volatile bool running = true;
-
-    // TODO: somehow the visual studio code debugger under linux
-    //       does not like multiThreading. maybe we can find out why.
-#if DEBUG
-    public const float ThreadsPerProcessor = 0.0f;
-#else
-    public const float ThreadsPerProcessor = 0.9f;
-#endif
 
     private volatile int tasksLeft;
     internal int threadCount;
@@ -108,13 +102,15 @@ public sealed class ThreadPool
         int initialThreadCount = ThreadCountSuggestion;
 
 #if !NET9_0_OR_GREATER
-        // Fixed in .NET >= 9 - hangups/freezing in Debug mode.
-        // https://github.com/dotnet/runtime/pull/95555
+        // .NET versions below 9.0 have a known issue that can cause hangups or freezing
+        // when debugging on non-Windows systems. See: https://github.com/dotnet/runtime/pull/95555
+        // To avoid this issue, multi-threading is disabled when a debugger is attached on non-Windows systems.
         if (!OperatingSystem.IsWindows() && Debugger.IsAttached)
         {
-            System.Diagnostics.Debug.WriteLine("Debugger + .NET version < 9.0 + non-Windows System detected: " +
-                                               "Disabled multi-threading!");
-            initialThreadCount = 1;
+            System.Diagnostics.Debug.WriteLine(
+                "Multi-threading disabled to prevent potential hangups: Debugger attached, " +
+                ".NET version < 9.0, non-Windows system detected.");
+            initialThreadCount = 1; // Forces single-threading to avoid hangups
         }
 #endif
 
