b03fe504b4d2f90baeb9543256f189789e73880a
notgiven688 <notgiven688@users.noreply.github.com>
2024-11-20 14:16:43 +0100

    Replace top-level await

--- other/WebDemo/main.js
+++ other/WebDemo/main.js
@@ -1,26 +1,31 @@
-import { dotnet } from './_framework/dotnet.js'
+import { dotnet } from './_framework/dotnet.js';
 
-const { getAssemblyExports, getConfig, runMain } = await dotnet
-    .withDiagnosticTracing(false)
-    .create();
+async function initialize() {
+    const { getAssemblyExports, getConfig, runMain } = await dotnet
+        .withDiagnosticTracing(false)
+        .create();
 
-const config = getConfig();
-const exports = await getAssemblyExports(config.mainAssemblyName);
+    const config = getConfig();
+    const exports = await getAssemblyExports(config.mainAssemblyName);
 
-dotnet.instance.Module['canvas'] = document.getElementById('canvas');
+    dotnet.instance.Module['canvas'] = document.getElementById('canvas');
 
-function mainLoop() {
-    exports.WebDemo.Application.UpdateFrame();
+    function mainLoop() {
+        exports.WebDemo.Application.UpdateFrame();
+        window.requestAnimationFrame(mainLoop);
+    }
+
+    // Run the C# Main() method and keep the runtime process running and executing further API calls
+    await runMain();
+
+    // Remove the spinner once the application is ready
+    const loading_div = document.getElementById('spinner');
+    loading_div.remove();
 
     window.requestAnimationFrame(mainLoop);
 }
 
-// https://github.com/dotnet/runtime/blob/74c608d67d64675ff840c5888368669777c8aa2c/src/mono/wasm/templates/templates/browser/wwwroot/main.js#L31-L32
-// run the C# Main() method and keep the runtime process running and executing further API calls
-await runMain();
-
-// We're ready to dotnet.run, so let's remove the spinner
-const loading_div = document.getElementById('spinner');
-loading_div.remove();
-
-window.requestAnimationFrame(mainLoop);
+// Initialize the application
+initialize().catch(err => {
+    console.error('An error occurred during initialization:', err);
+});
