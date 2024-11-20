a1bd3f1f37e0ee2eed4b1947ac20fb876baba484
notgiven688 <notgiven688@users.noreply.github.com>
2024-11-20 13:43:03 +0100

    Update the webdemo

--- docs/static/AppBundle/main.js
+++ docs/static/AppBundle/main.js
@@ -1,19 +1,26 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-import { dotnet } from './dotnet.js';
+import { dotnet } from './_framework/dotnet.js'
 
-(async () => {
-  await dotnet
-    .withDebugging(1)
+const { getAssemblyExports, getConfig, runMain } = await dotnet
     .withDiagnosticTracing(false)
-    .withApplicationArgumentsFromQuery()
     .create();
 
-  dotnet.instance.Module['canvas'] = document.getElementById('canvas');
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
 
-  // We're ready to dotnet.run, so let's remove the spinner
-  const loading_div = document.getElementById('spinner');
-  loading_div.remove();
+dotnet.instance.Module['canvas'] = document.getElementById('canvas');
 
-  await dotnet.run();
-})();
+function mainLoop() {
+    exports.WebDemo.Application.UpdateFrame();
+
+    window.requestAnimationFrame(mainLoop);
+}
+
+// https://github.com/dotnet/runtime/blob/74c608d67d64675ff840c5888368669777c8aa2c/src/mono/wasm/templates/templates/browser/wwwroot/main.js#L31-L32
+// run the C# Main() method and keep the runtime process running and executing further API calls
+await runMain();
+
+// We're ready to dotnet.run, so let's remove the spinner
+const loading_div = document.getElementById('spinner');
+loading_div.remove();
+
+window.requestAnimationFrame(mainLoop);
