4849318121ceed610ea512f42617a59eb47f0d87
notgiven688 <notgiven688@users.noreply.github.com>
2024-11-25 19:32:21 +0100

    Add ConstraintTests.cs

---
+++ src/JitterTests/ConstraintTests.cs
@@ -0,0 +1,34 @@
+using Jitter2.Dynamics.Constraints;
+using Jitter2.SoftBodies;
+
+namespace JitterTests;
+
+public class ConstraintTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase]
+    public void TestSizes()
+    {
+        World world = new World();
+
+        RigidBody b0 = world.CreateRigidBody();
+        RigidBody b1 = world.CreateRigidBody();
+
+        world.Remove(world.CreateConstraint<AngularMotor>(b0, b1));
+        world.Remove(world.CreateConstraint<BallSocket>(b0, b1));
+        world.Remove(world.CreateConstraint<ConeLimit>(b0, b1));
+        world.Remove(world.CreateConstraint<DistanceLimit>(b0, b1));
+        world.Remove(world.CreateConstraint<FixedAngle>(b0, b1));
+        world.Remove(world.CreateConstraint<HingeAngle>(b0, b1));
+        world.Remove(world.CreateConstraint<LinearMotor>(b0, b1));
+        world.Remove(world.CreateConstraint<PointOnLine>(b0, b1));
+        world.Remove(world.CreateConstraint<PointOnPlane>(b0, b1));
+        world.Remove(world.CreateConstraint<TwistAngle>(b0, b1));
+        world.Remove(world.CreateConstraint<SpringConstraint>(b0, b1));
+    }
+
+}
