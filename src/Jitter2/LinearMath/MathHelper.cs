6d6d44c05cab4783b24a17207755950e8e92f630
notgiven688 <notgiven688@users.noreply.github.com>
2025-06-02 22:51:01 +0200

    Refactor DynamicTree: remove Height property and optimize insertion

--- src/Jitter2/LinearMath/MathHelper.cs
+++ src/Jitter2/LinearMath/MathHelper.cs
@@ -137,11 +137,9 @@ public static class MathHelper
     /// </summary>
     public static bool IsZero(in JVector vector, Real epsilon = (Real)1e-6)
     {
-        Real x = MathR.Abs(vector.X);
-        Real y = MathR.Abs(vector.Y);
-        Real z = MathR.Abs(vector.Z);
-
-        return MathR.Max(x, MathR.Max(y, z)) < epsilon;
+        return !(MathR.Abs(vector.X) >= epsilon) &&
+               !(MathR.Abs(vector.Y) >= epsilon) &&
+               !(MathR.Abs(vector.Z) >= epsilon);
     }
 
     /// <summary>
