56458881ba9503e454423a946cb2a1b73ef08c8b
notgiven688 <notgiven688@mail.com>
2026-02-20 21:10:08 +0100

    Use Free(void*) in xml docs in MemoryHelper

--- src/Jitter2/Unmanaged/MemoryHelper.cs
+++ src/Jitter2/Unmanaged/MemoryHelper.cs
@@ -15,7 +15,7 @@ namespace Jitter2.Unmanaged;
 /// <remarks>
 /// <para>
 /// <b>Safety:</b> All pointers returned by allocation methods must be freed using
-/// the corresponding <see cref="Free(System.Void*)"/> or <see cref="AlignedFree"/> method.
+/// the corresponding <see cref="Free(void*)"/> or <see cref="AlignedFree"/> method.
 /// Failure to do so will result in memory leaks.
 /// </para>
 /// <para>
@@ -129,7 +129,7 @@ public static unsafe class MemoryHelper
     /// <param name="len">The length of the memory block to allocate, in bytes.</param>
     /// <returns>A pointer to the allocated memory block.</returns>
     /// <remarks>
-    /// <b>Safety:</b> The caller must free the returned pointer using <see cref="Free(System.Void*)"/>.
+    /// <b>Safety:</b> The caller must free the returned pointer using <see cref="Free(void*)"/>.
     /// The memory is not zero-initialized.
     /// </remarks>
     public static void* AllocateHeap(int len) => NativeMemory.Alloc((nuint)len);
